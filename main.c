7d66c88a8a84310493e687cd515e264f44b901db
Guilherme <batistaguilherme2002@gmail.com>
2024-03-11 19:53:46 -0300

    Melhorando mensagens ao usuário para maior clareza e compreensão

--- main.c
+++ main.c
@@ -29,15 +29,10 @@
     //Recebe a quantidade de atividades propostas para compor a N1
         do {
 
-            printf("\nQuantas atividades foram propostas para compor a N1? ");
+            printf("\nQuantas atividades foram aplicadas na N1? ");
+            printf("\nNúmero máximo de atividades permitido é 4.");
             scanf("%i",&Tam_Vetor);
             printf("\n");
-
-            if(Tam_Vetor<0||Tam_Vetor>4)
-            {
-                system("cls");
-                printf("Digite apenas números entre 0 e 4...");
-            }
 
         } while(Tam_Vetor<0||Tam_Vetor>4);
         
@@ -46,50 +41,31 @@
         {
             do {
                 
-                printf("Quanto tirou na A%i? ", cont+1);
+                printf("Qual foi sua nota na atividade A%i? De 0 a 10", cont+1);
                 scanf("%f",&notaN1[cont]);
                 system("cls");
 
                 N1 += (notaN1[cont]/Tam_Vetor)*0.4;
 
-                if(notaN1[cont]<0||notaN1[cont]>10)
-                {
-                    printf("Digite a nota de 0 a 10.\n");
-                    printf("\n");
-                }
-
             } while (notaN1[cont]<0||notaN1[cont]>10);
         }
-    
     
     //Recebe a nota da N2 e APS
         do {
             
-            printf("\nQuanto tirou na N2? ");
+            printf("\nQual foi sua nota na N2? De 0 a 9");
             scanf("%f",&N2);
             N2 *= 0.6;
             system("cls");
-
-            if (N2<0||N2>9)
-            {
-                printf("N2 tem pontuação de 0 a 9 por conta da APS.");
-                printf("\n");
-            }
 
         } while (N2<0||N2>9);
         
         do {
 
-            printf("\nQuanto tirou na APS? ");
+            printf("\nQual foi sua nota na APS? Entre 0 e 1");
             scanf("%f",&APS);
             N2+=APS;
             system("cls");
-
-            if(APS<0||APS>1)
-            {
-                printf("Digite a nota de 0 a 1.");
-                printf("\n");
-            }
 
         } while (APS<0||APS>1);
         
@@ -101,7 +77,7 @@
         if (Media>=6)
         {
             imprime_media(Media);
-            printf("\nParabéns, você atingiu a média necessária!\n");
+            printf("\nParabéns! Você atingiu a média exigida para aprovação!\n");
             printf("\n");   
         } else 
             {
@@ -110,12 +86,12 @@
                 if (Media>=6)
                 {
                     imprime_media(Media);
-                    printf("\nParabéns, você conseguiu a média necessária.\nUfaaa.. Essa foi por pouco!!");
+                    printf("\nParabéns! Você atingiu a média exigida para aprovação!");
                     printf("\n");     
                 } else 
                     {
                         imprime_media(Media);
-                        printf("\nInfelizmente você ficou de DP..");
+                        printf("\nInfelizmente, Você não atingiu a média exigida para aprovação.");
                         printf("\n");
                     }
             }
@@ -148,9 +124,7 @@
         char check;
         imprime_media(M);
         printf("\nMédia final inferior a necessária. ");
-        printf("Realizou a substutiva? ");
-        printf("\n");
-        printf("Digite (S) para SIM ou (N) para NÃO: ");
+        printf("Realizou a substutiva? (S/N): ");
         scanf(" %c",&check);
 
         switch (check)
